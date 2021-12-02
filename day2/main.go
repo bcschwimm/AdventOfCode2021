ec4c36509ef2bb3d9531f9af85ecc5a40fab80e5
Bill Coll <59691769+bcschwimm@users.noreply.github.com>
2021-12-02 16:13:38 -0500

    Add files via upload

---
+++ day2/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// readInput reads our text file
+// and returns an array of ints
+func readInput() []string {
+	file, err := os.Open("input.txt")
+	defer file.Close()
+
+	if err != nil {
+		log.Fatalf("failed to open file")
+	}
+
+	scanner := bufio.NewScanner(file)
+	var text []string
+
+	for scanner.Scan() {
+		text = append(text, scanner.Text())
+	}
+
+	return text
+}
+
+func strToInt(s string) int {
+	val, err := strconv.Atoi(s)
+
+	if err != nil {
+		fmt.Println("Could not convert string to int", err)
+	}
+	return val
+}
+
+func answerOne(input []string) int {
+	var horizontal int
+	var depth int
+
+	for _, value := range input {
+		data := strings.Fields(value)
+
+		switch direction := data[0]; direction {
+		case "forward":
+			horizontal += strToInt(data[len(data)-1])
+		case "down":
+			depth += strToInt(data[len(data)-1])
+		case "up":
+			depth -= strToInt(data[len(data)-1])
+		default:
+			fmt.Println("Could not translate instructions")
+		}
+	}
+	return horizontal * depth
+}
+
+func answerTwo(input []string) int {
+	var horizontal int
+	var depth int
+	var aim int
+
+	for _, value := range input {
+		data := strings.Fields(value)
+
+		switch direction := data[0]; direction {
+		case "forward":
+			horizontal += strToInt(data[len(data)-1])
+			depth += (strToInt(data[len(data)-1]) * aim)
+		case "down":
+			aim += strToInt(data[len(data)-1])
+		case "up":
+			aim -= strToInt(data[len(data)-1])
+		default:
+			fmt.Println("Could not translate instructions")
+		}
+	}
+	return horizontal * depth
+}
+
+func main() {
+	inputData := readInput()
+
+	firstAnswer := answerOne(inputData)
+	secondAnswer := answerTwo(inputData)
+	fmt.Println(firstAnswer, secondAnswer)
+}
